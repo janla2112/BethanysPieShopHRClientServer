cb111e23b367fedca1939eeea79fcd02224c83d3
Jan Lawrence <Jan.Lawrence@ct.gov>
2020-05-01 11:01:27 -0400

    Add project files.

---
+++ BethanysPieShopHRM.Client/Startup.cs
@@ -0,0 +1,31 @@
+using System.Net.Http;
+using BethanysPieShopHRM.Server;
+using BethanysPieShopHRM.Server.Services;
+using Microsoft.AspNetCore.Builder.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace BethanysPieShopHRM.ClientApp
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<HttpClient>(s =>
+            {
+                var client = new HttpClient(){ BaseAddress = new System.Uri("https://localhost:44340/") };
+                return client;
+            });
+
+            //services.AddScoped<IEmployeeDataService, MockEmployeeDataService>();
+            services.AddScoped<IEmployeeDataService, EmployeeDataService>();
+            services.AddScoped<ICountryDataService, CountryDataService>();
+            services.AddScoped<IJobCategoryDataService, JobCategoryDataService>();
+        }
+
+        public void Configure(IComponentsApplicationBuilder app)
+        {
+            app.AddComponent<App>("app");
+        }
+    }
+}
